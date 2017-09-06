685803fe2c0868aee3df19b8bb6216d08d5f9bd5
Artem Govorov <artem.govorov@gmail.com>
2017-09-06 15:06:02 +1000

    Update wallaby.conf.js

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,6 +1,13 @@
+var path = require('path');
 var wallabyWebpack = require('wallaby-webpack');
 var wallabyPostprocessor = wallabyWebpack({
-  entryPatterns: ['test/unit/setup.js', 'test/unit/**/*.spec.js']
+  entryPatterns: ['test/unit/setup.js', 'test/unit/**/*.spec.js'],
+  resolve: {
+    alias: {
+      'kendo-ui/js': '@progress/kendo-ui/js',
+      'kendo-ui': '@progress/kendo-ui/js/kendo.core.js'
+    }
+  }
 });
 
 module.exports = function () {
